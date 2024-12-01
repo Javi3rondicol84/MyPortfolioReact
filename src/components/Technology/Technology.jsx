3ce67d8fbf5134366da8361fd1fcc69ec85cd385
javier <javierse9@gmail.com>
2024-12-01 16:03:27 -0300

    technologies card done

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -8,23 +8,31 @@ const Technology = ({nameTech, image, category}) => {
         <>
         <div className='techCard'>
             <div className='techCardContent'>
-                <div className='techCategory'>
-                    <h3>Category: </h3>
-                    <span>{category}</span>
-                </div>
-                <div className='techImgName'>
-                    <div className='techImg'>
+                <div className='techImgSection'>
+                    <div>
                         <img src={image} alt={nameTech} />
                     </div>
-                    <div className='techName'>
-                        <span>{nameTech}</span>
+                </div>
+                <div className='techCardContentInfo'>
+                    <div className='techCategory'>
+                        <h3>Category: </h3>
+                        <span>{category}</span>
+                    </div>
+                    <div className='techNameSection'>
+                        <div className='techTitle'>
+                            <span>Name: </span>
+                        </div>
+                        <div className='techName'>
+                            <span>{nameTech}</span>
+                        </div>
+                    </div>
+                    <div className='techLink'>
+                        <a href='#App'>
+                        <div className='titleSearchImg'><span>Projects with: </span></div>
+                        <div className='searchImg'><img src={SearchProjects} alt='searchProjects'/></div></a>
                     </div>
                 </div>
-                <div className='techLink'>
-                    <a href='#App'>
-                    <div className='titleSearchImg'><span>Projects with: </span></div>
-                    <div className='searchImg'><img src={SearchProjects} alt='searchProjects'/></div></a>
-                </div>
+                
             </div>
         </div>
         
